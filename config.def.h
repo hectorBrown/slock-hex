6f79d5a066ad51a5b93aacb98ae3c687558381b8
Hector Brown <hectorjbrown@protonmail.com>
2023-05-22 23:56:29 +0100

    config

--- config.def.h
+++ config.def.h
@@ -3,20 +3,20 @@ static const char *user  = "hex";
 static const char *group = "hex";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[BG] = "black",
-	[INPUT] =  "#005577",   /* during input */
-	[INPUT_ALT] = "#227799", /* during input, second color */
-	[FAILED] = "#CC3333",   /* wrong password */
-	[CAPS] = "red",         /* CapsLock on */
-	[PAM] =    "black",   /* waiting for PAM */
+	[INIT] =   "#242424",     /* after initialization */
+	[BG] = "black", /* pretty sure this isn't ever displayed */
+	[INPUT] =  "#689D6A",   /* during input */
+	[INPUT_ALT] = "#8EC07C", /* during input, second color */
+	[FAILED] = "#CC241D",   /* wrong password */
+	[CAPS] = "#D79921",         /* CapsLock on */
+	[PAM] =    "#928374",   /* waiting for PAM */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
 
 /* size of square in px */
-static const int squaresize = 50;
+static const int squaresize = 200;
 
 /* PAM service that's used for authentication */
 static const char* pam_service = "login";
@@ -32,8 +32,8 @@ static const char *command = "/usr/bin/xset dpms force off";
 /*Enable blur*/
 #define BLUR
 /*Set blur radius*/
-static const int blurRadius=5;
+static const int blurRadius=30;
 /*Enable Pixelation*/
-//#define PIXELATION
+/*#define PIXELATION*/
 /*Set pixelation radius*/
 static const int pixelSize=0;
