d96035e0a5ccbaf7a39f4b39d1b923b193b9e5c2
Hector Brown <hectorjbrown@protonmail.com>
2023-05-22 23:03:00 +0100

    pam auth

--- config.def.h
+++ config.def.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nobody";
+static const char *user  = "hex";
+static const char *group = "hex";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
@@ -8,6 +8,7 @@ static const char *colorname[NUMCOLS] = {
 	[INPUT] =  "#005577",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 	[CAPS] = "red",         /* CapsLock on */
+	[PAM] =    "black",   /* waiting for PAM */
 };
 
 /* treat a cleared input like a wrong password (color) */
@@ -15,3 +16,6 @@ static const int failonclear = 1;
 
 /* size of square in px */
 static const int squaresize = 50;
+
+/* PAM service that's used for authentication */
+static const char* pam_service = "login";
